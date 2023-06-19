0057179d07a2ab3b3b4c3e6ed63225bd230ad2e6
Fahrettin Baştürk <fahrettin_basturk@hotmail.com>
2023-06-19 13:01:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import pytesseract
 from tkinter import filedialog
 import tkinter as tk
+import os
 
 # Path to the Tesseract executable file
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -10,22 +11,24 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 root = tk.Tk()
 root.withdraw()
 
-# Ask the user to select a file
-png_file_path = filedialog.askopenfilename(title='Select PNG file', filetypes=[('PNG Files', '*.png')])
+# Ask the user to select multiple files
+png_file_paths = filedialog.askopenfilenames(title='Select PNG files', filetypes=[('PNG Files', '*.png')])
 
-# Check if a file was selected
-if png_file_path:
-    # Open the selected PNG file and read the text
-    with Image.open(png_file_path) as img:
-        text = pytesseract.image_to_string(img, lang='eng')
+# Check if files were selected
+if png_file_paths:
+    for png_file_path in png_file_paths:
+        # Open each selected PNG file and read the text
+        with Image.open(png_file_path) as img:
+            text = pytesseract.image_to_string(img, lang='eng')
 
-    # Path to the output file
-    output_file_path = 'metin.txt'
+        # Create a unique output file path for each selected file
+        base_filename = os.path.splitext(os.path.basename(png_file_path))[0]
+        output_file_path = base_filename + '.txt'
 
-    # Write the text to the output file
-    with open(output_file_path, mode='w', encoding='utf-8') as file:
-        file.write(text)
+        # Write the text to the output file
+        with open(output_file_path, mode='w', encoding='utf-8') as file:
+            file.write(text)
 
-    print('The text in the PNG file has been successfully written to the output file.')
+        print(f"The text in {png_file_path} has been successfully written to {output_file_path}.")
 else:
-    print('No file selected.')
+    print('No files selected.')
